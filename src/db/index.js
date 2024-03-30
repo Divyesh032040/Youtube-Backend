3231009b74f4c7254c9a78cc7faa312efcaf21dc
Divyesh Parmar <dp032040@gmail.com>
2024-03-30 18:04:18 +0530

    databse connected

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,11 @@
+
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import { DB_NAME } from "../constants.js"
 
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
@@ -13,3 +14,4 @@ const connectDB = async () => {
 }
 
 export default connectDB
+
