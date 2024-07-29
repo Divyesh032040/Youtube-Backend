d41b2a7f707bc645b68f5d8eeed5a3c248528f42
Divyesh Parmar <127118175+Divyesh032040@users.noreply.github.com>
2024-07-29 07:32:16 +0530

    Update ApiResponse.js

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,9 +1,9 @@
-class ApiResponse {
+ ApiResponse {
     constructor(statusCode, data, message = "Success"){
         this.statusCode = statusCode
         this.data = data
         this.message = message
-        this.success = statusCode < 400
+       // this.success = statusCode < 400
     }
 }
 
